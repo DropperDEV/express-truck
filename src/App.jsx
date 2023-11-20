d1ff28a4f83740a217fe5953243060d78131d497
dropperDEV <jamescosta4747@Gmail.com>
2023-11-19 22:53:42 -0300

    files empty created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import Home from "./ui/Home";
+import Terms from "./features/terms/Terms";
+import Account from "./features/account/Account";
+import About from "./features/about/About";
+import Partners from "./features/partners/Partners";
+import Login from "./features/account/Login";
+import SignUp from "./features/account/SignUp";
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        { path: "/", element: <Home />, errorElement: <Error /> },
+        { path: "/terms", element: <Terms />, errorElement: <Error /> },
+        {
+          path: "/account/myaccount",
+          element: <Account />,
+          errorElement: <Error />,
+        },
+        { path: "/account/login", element: <Login />, errorElement: <Error /> },
+        {
+          path: "/account/signup",
+          element: <SignUp />,
+          errorElement: <Error />,
+        },
+        { path: "/about", element: <About />, errorElement: <Error /> },
+        { path: "/partners", element: <Partners />, errorElement: <Error /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
