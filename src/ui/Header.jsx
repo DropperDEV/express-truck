5cd596ab9198dfba33d8742a658fcec3afd2d11b
dropperDEV <jamescosta4747@Gmail.com>
2023-11-20 00:02:27 -0300

    feat: responsived header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import LoginIcon from "./../features/account/LoginIcon";
 export default function Header() {
   return (
     <header className="flex items-center justify-around bg-stone-950 px-4 py-4 sm:px-6">
-      <h1 className=" text-center text-xl font-bold tracking-normal  text-slate-50">
+      <h1 className=" text-center text-base font-bold tracking-normal text-slate-50  sm:text-xl lg:text-2xl">
         Express <br />
         <span className=" bg-slate-50 text-stone-950">
           Tru<span className="text-blue-700">ck</span>
@@ -14,7 +14,8 @@ export default function Header() {
         {" "}
         <input
           placeholder="Search..."
-          className=" w-32 rounded-full px-3 py-2  text-sm sm:px-8 sm:py-5"
+          className=" w-32 rounded-full px-3 py-3 text-sm  transition-all duration-700  focus:outline-none focus:ring-4
+          focus:ring-blue-400 focus:ring-opacity-50 sm:w-44 sm:px-5  sm:py-4 sm:text-base sm:focus:w-72 lg:w-60 lg:focus:w-80 2xl:w-80 2xl:focus:w-96"
         />
       </div>
 
