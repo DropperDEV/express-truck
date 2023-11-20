ed0ae6b7c4254eec2ea77114d1605f46d87779fe
dropperDEV <jamescosta4747@Gmail.com>
2023-11-19 23:19:09 -0300

    feat: header structure made

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,16 @@
+import LoginIcon from "./../features/account/LoginIcon";
 
 export default function Header() {
   return (
-    <header className="bg-pink-600">
+    <header className="flex items-center justify-around bg-pink-600 px-4 py-3 sm:px-6">
+      <h1>Express Truck</h1>
 
+      <div>
+        {" "}
+        <input placeholder="Search..." className="" />
+      </div>
+
+      <LoginIcon />
     </header>
-  )
+  );
 }
