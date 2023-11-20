65e47e70d2badd030306aab49f510a53ccd71b56
dropperDEV <jamescosta4747@Gmail.com>
2023-11-19 23:44:07 -0300

    feat: header

--- src/features/account/LoginIcon.jsx
+++ src/features/account/LoginIcon.jsx
@@ -2,7 +2,7 @@ export default function LoginIcon() {
   return (
     <svg
       width="100"
-      height="100"
+      height="70"
       viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,12 +2,20 @@ import LoginIcon from "./../features/account/LoginIcon";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-around bg-pink-600 px-4 py-3 sm:px-6">
-      <h1>Express Truck</h1>
+    <header className="flex items-center justify-around bg-stone-950 px-4 py-4 sm:px-6">
+      <h1 className=" text-center text-xl font-bold tracking-normal  text-slate-50">
+        Express <br />
+        <span className=" bg-slate-50 text-stone-950">
+          Tru<span className="text-blue-700">ck</span>
+        </span>
+      </h1>
 
       <div>
         {" "}
-        <input placeholder="Search..." className="" />
+        <input
+          placeholder="Search..."
+          className=" w-32 rounded-full px-3 py-2  text-sm sm:px-8 sm:py-5"
+        />
       </div>
 
       <LoginIcon />
