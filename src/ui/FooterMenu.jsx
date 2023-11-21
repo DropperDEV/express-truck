e7d64f71e9afd669a1d640bf28d7694acfd0375b
dropperDEV <jamescosta4747@Gmail.com>
2023-11-20 23:44:01 -0300

    footer desktop finished

--- src/ui/FooterMenu.jsx
+++ src/ui/FooterMenu.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export default function FooterMenu({children}) {
-  return <Link className=" bg-blue-1000 uppercase text-lg flex items-center justify-center">{children}</Link>;
+export default function FooterMenu({ children }) {
+  return (
+    <Link className=" flex items-center justify-center bg-blue-1000 uppercase">
+      {children}
+    </Link>
+  );
 }
--- src/ui/FooterSlide.jsx
+++ src/ui/FooterSlide.jsx
@@ -1,4 +1,4 @@
 /* eslint-disable react/prop-types */
 export default function FooterSlide({children}) {
-  return <div className=" bg-blue-1000 w-full py-6 grid grid-cols-3 text-lg">{children}</div>;
+  return <div className=" bg-blue-1000 w-full py-6 grid grid-cols-3 text-xl">{children}</div>;
 }
