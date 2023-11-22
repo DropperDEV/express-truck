9e0f8b44737d2b957528800143bf87621c7fec5b
dropperDEV <jamescosta4747@Gmail.com>
2023-11-21 23:44:24 -0300

    centerTtile home made

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -2,7 +2,7 @@
 
 export default function Card({ image, altImage, name }) {
   return (
-    <div className=" flex  w-52 flex-col  items-center justify-center gap-10 place-self-center rounded-2xl  bg-violet-1000 px-3 py-8 text-lg font-semibold text-slate-50 sm:px-8 sm:py-14 sm:text-xl md:px-8 md:py-20 md:text-2xl">
+    <div className=" my-20  flex w-52 gap-10  flex-col items-center justify-center  place-self-center  rounded-2xl bg-violet-1000 px-3 py-8 text-lg font-semibold text-slate-50 sm:px-8 sm:py-14 sm:text-xl md:px-8 md:py-20 md:text-2xl">
       <img
         src={image}
         alt={altImage}
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,15 +1,21 @@
 import Card from "./Card";
-import Banner from "./Banner"
+import Banner from "./Banner";
 
 export default function Home() {
   return (
     <div>
-      <Banner/>
-      <section className=" grid grid-cols-1 gap-10 md:flex md:flex-row md:items-center md:justify-around md:gap-0  ">
+      <Banner />
+      <section className=" grid grid-cols-1  md:flex md:flex-row md:items-center md:justify-around md:gap-0  ">
         <Card image="user.png" altImage="Usuário" name="Área Cliente" />
         <Card image="terms.jpg" altImage="Termos" name="Termos" />
         <Card image="partners.jpg" altImage="Parceiros" name="Parceiros" />
       </section>
+      <div className="mt-36  flex h-full w-full items-center justify-center bg-center">
+        <img src="trucks.png" className="w-5/6 md:w-4/6 lg:w-3/6" />
+      </div>
+      <div className=" flex items-center justify-center bg-slate-950 p-10 py-16 text-center text-xl font-bold text-slate-50 sm:text-2xl">
+        <p>Os mais rápidos e experientes do mercado</p>
+      </div>
     </div>
   );
 }
