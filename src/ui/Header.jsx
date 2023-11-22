5a4fa14133a92acb911f179a8c12d4e0f2882f92
dropperDEV <jamescosta4747@Gmail.com>
2023-11-21 23:29:08 -0300

    text header font change

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import LoginIcon from "./../features/account/LoginIcon";
 export default function Header() {
   return (
     <header className="flex items-center justify-around bg-stone-950 px-4 py-4 sm:px-6 w-full">
-      <h1 className=" text-center text-base font-bold tracking-normal text-slate-50  sm:text-xl lg:text-2xl">
+      <h1 className=" text-center text-xl font-bold tracking-normal text-slate-50 sm:text-2xl lg:text-3xl">
         Express <br />
         <span className=" bg-slate-50 text-stone-950">
           Tru<span className="text-blue-700">ck</span>
