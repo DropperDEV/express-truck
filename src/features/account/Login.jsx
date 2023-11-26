e262f460096eb700c3be1526ad1c7399dd62d6f1
dropperDEV <jamescosta4747@Gmail.com>
2023-11-26 11:06:16 -0300

    left side login

--- src/features/account/Login.jsx
+++ src/features/account/Login.jsx
@@ -1,6 +1,28 @@
+import Button from "./../../ui/Button";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="flex flex-row items-center justify-center">
+      <div className="flex flex-col  rounded-xl bg-blue-1100 p-10 text-slate-50">
+        <div className="flex w-fit flex-col gap-7 px-12 pt-14">
+          <p className=" text-6xl font-semibold">
+            Bem <br className="mb-4" /> Vindo!
+          </p>
+          <Button text="Entrar" login={true} />
+          <p className="mt-4 text-center">Voltar ao iníco</p>
+        </div>
+      </div>
+      <div>
+        <h1>Login</h1>
+        <form action="">
+          <div>
+            <input type="text" placeholder="email" />
+            <input type="text" placeholder="senha" />
+            <input type="text" placeholder="cpf" />
+          </div>
+          <p>Não possui conta? Cadastre aqui.</p>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
-export default function Button({text}) {
+export default function Button({text, login}) {
+  if(login === true) return  <button className="bg-slate-50 text-stone-950 py-4 px-10 rounded-2xl">{text}</button>
   return (
     <button className="bg-violet-1000 text-slate-50 py-4 px-10 rounded-2xl">{text}</button>
   )
