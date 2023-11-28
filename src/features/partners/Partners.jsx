b8321024f1963b33d7dad8e8898156dc3529293c
dropperDEV <jamescosta4747@Gmail.com>
2023-11-27 22:55:38 -0300

    feat: partner title slide

--- src/features/partners/Partners.jsx
+++ src/features/partners/Partners.jsx
@@ -1,6 +1,16 @@
-
 export default function Partners() {
   return (
-    <div>Partners</div>
-  )
+    <div className="relative mt-44">
+      <div className=" relative z-10 ml-[25%] w-3/4 overflow-visible py-4">
+        <div className="absolute inset-0 -ml-8 bg-[#033B75] p-8">&nbsp;</div>
+      </div>
+      <div className="z-1 relative bg-stone-950 p-14 text-slate-50">
+        <div className="border-l-2 border-slate-50 p-8">
+          <h1 className="text-4xl font-semibold uppercase md:text-5xl lg:text-6xl">
+            Seja parceiro
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
