82e6d95524477f59fe1decfc61fc98be5ad83f13
dropperDEV <jamescosta4747@Gmail.com>
2023-12-08 20:30:47 -0300

    feat: register visible password

--- src/features/account/GoTo.jsx
+++ src/features/account/GoTo.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function GoTo({ text, route }) {
   return (
     <Link to={route} className="sm:mt-4">
-      <p className=" text-center text-base   font-semibold text-slate-50">
+      <p className=" text-center text-sm   font-semibold text-slate-50">
         {text}
       </p>
     </Link>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,10 +6,10 @@ import { useState } from "react";
 import { useLogin } from "../features/account/useLogin";
 import RegisterCol from "./../features/account/RegisterCol";
 import RegisterInput from "./../features/account/RegisterInput";
-
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [visiblePassword, setVisiblePassword] = useState(false);
 
   const { login } = useLogin();
 
@@ -47,11 +47,22 @@ export default function Login() {
           />
           <RegisterInput
             text="Senha"
-            type="password"
+            type={visiblePassword ? "text" : "password"}
             name={"password"}
             action={(e) => setPassword(e.target.value)}
             value={password}
           />
+          <div className="  sm:mr-24 lg:mr-48 flex  gap-2">
+            <input
+              onChange={() => setVisiblePassword(!visiblePassword)}
+              id="passwordTurnVisible"
+              type="checkbox"
+              name="passwordTurnVisible"
+              value=""
+              className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+            />
+            <p className=" text-sm text-slate-50">Mostrar a senha?</p>
+          </div>
           <GoTo
             text="Não possui conta? Cadastre aqui."
             route="/account/signup"
